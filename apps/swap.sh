bd11613320c70b57f8f9d502c2cbeddcb2eb69b5
EuanEdgar <euan@spicerack.co.uk>
2018-10-16 13:46:40 +0100

    Add basic info about swap

--- apps/swap.sh
+++ apps/swap.sh
@@ -4,8 +4,61 @@
 exit_teacup_not_empty=1
 exit_arguments_missing=2
 exit_file_not_found=3
+exit_invalid_options=4
 
 teacup_location='/tmp/ee_swap'
+
+usage() {
+  echo "Swap:
+  Swaps the names of two files or directories
+  Usage: swap path/to/file1 path/to/file/2
+  Options:
+    -h: Show this message"
+}
+
+print_version() {
+  echo 'Just be glad it exists at all'
+}
+
+get_long_option() {
+  case $1 in
+    help)
+      usage
+      exit 0
+      ;;
+    version)
+      print_version
+      exit 0
+      ;;
+  esac
+}
+
+while getopts ":h:v:-:" opt; do
+  case $opt in
+    -)
+      get_long_option "$OPTARG"
+      exit 0
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+    v)
+      print_version
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      exit $exit_invalid_options
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      exit $exit_invalid_options
+      ;;
+  esac
+done
 
 # if cleanup not performed
 if [[ -a $teacup_location ]] ; then
