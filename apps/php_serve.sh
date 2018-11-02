d20dc9b96ee50fbbcfea653a6a87275ec86cfbc6
EuanEdgar <euan@spicerack.co.uk>
2018-11-02 16:34:13 +0000

    Add more error handling to php_serve

--- apps/php_serve.sh
+++ apps/php_serve.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-exit_missing_port=1
+# exit code 1 is a php error
+exit_missing_port=2
+exit_invalid_port=3
+
+usage(){
+  echo "Devserve:
+  Starts a development server on a given port number
+  Usage: devserve [port:n+]"
+}
 
 port=$1
 
 if [ -z $port ] ; then
   usage
   exit $exit_missing_port
+fi
+
+if [[ ! $port =~ ^[0-9]+$ ]] ; then
+  usage
+  exit $exit_invalid_port
 fi
 
 line=$(ifconfig | grep 192 | head -n 1)
