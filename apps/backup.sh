3cf5c86d1296a2d37fa75142f6ec83a4b332eee6
EuanEdgar <euan@spicerack.co.uk>
2021-06-04 16:57:46 +0100

    Backup restore prompts for choice

--- apps/backup.sh
+++ apps/backup.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+getChoice() {
+  {
+    local prompt=${1}
+    shift
+
+    echo
+    printf '%s\0' "$@" | xargs -0 -n 1 basename | nl -s ': '
+    echo
+
+    local choice=0
+    while [[ ! ${choice} =~ ^[0-9]+$ ]] || (( choice < 1 || choice > $# )) ; do
+      read -re -p "${prompt}: " choice
+    done
+
+    return $(( choice - 1 ))
+  } >&2
+}
+
 backup(){
   file=$1
 
@@ -26,23 +44,37 @@ backup(){
 restore(){
   file=$1
 
-  if [ -f $file ] && [[ $file =~ (.*)\.bak([0-9]+)?$ ]]; then
-    basefile=$(echo "${BASH_REMATCH[1]}")
-    filepattern="$basefile.bak*"
-    read -p "This will overwrite $basefile and $(ls $filepattern | wc -l | xargs) backup file(s). Continue? ([Y]es/[N]o/[K]eep backup) " -r
-    if [[ $REPLY == [yY] ]]; then
-      mv $file $basefile
-      if [ ! -z $(for f in $filepattern; do
-        [ -e "$f" ] && echo 'found'
-        break
-      done) ]; then
-        rm $filepattern
+  if [ -f $file ]; then
+    if [[ $file =~ (.*)\.bak([0-9]+)?$ ]]; then
+      basefile=$(echo "${BASH_REMATCH[1]}")
+      backup=$file
+    else
+      basefile=$file
+
+      backups=( ${basefile}.bak* )
+
+      if [ -z "$(find . -maxdepth 1 -type f -name "${basefile}.bak*" 2>/dev/null)" ]; then
+        echo "No backups of ${file} found."
+        exit 1
+      else
+        getChoice "Choose a backup to restore" "${backups[@]}"
+        backup=${backups[$?]}
       fi
-    elif [[ $REPLY == [kK] ]]; then
-      cp $file $basefile
     fi
-  elif [ -f $file ]; then
-    echo "Restore file..."
+
+    filepattern="$basefile.bak*"
+
+    read -p "This will overwrite $basefile and $(ls $filepattern | wc -l | xargs) backup file(s). Continue? ([Y]es/[N]o/[K]eep backup) " -r
+
+    if [[ $REPLY == [yY] ]]; then
+      mv $backup $basefile
+    elif [[ $REPLY == [kK] ]]; then
+      cp $backup $basefile
+    fi
+
+    if [[ $REPLY == [yY] ]]; then
+      rm -f $filepattern
+    fi
   else
     echo "$file is not a backup"
   fi
